527bb261e0a02395a65e1fb50928412cb5fb2b25
CODER-SV <113542416+CODER-SV@users.noreply.github.com>
2023-01-18 19:21:49 +0530

    okay

---
+++ src/Article.jsx
@@ -0,0 +1,15 @@
+var react = require("react");
+
+function Article() {
+  return (
+    <div>
+      <p>
+        React makes it painless to create interactive UIs. Design simple views
+        for each state in your application, and React will efficiently update
+        and render just the right components when your data changes.
+      </p>
+    </div>
+  );
+}
+
+export default Article;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+import Heading from "./Heading";
+import Article from "./Article";
 var React = require("react");
 var ReactDOM = require("react-dom");
 
 ReactDOM.render(
   <div>
-    <h1> Hello World </h1>
-    <p>
-      React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
-    </p>
+    <Heading />
+    <Article />
   </div>,
   document.getElementById("root")
-)
+);
